fffef81dd3c072a3b6670a9e95002ac3ff51c50b
Max Garanin <m_garanin@mail.ru>
2019-10-27 11:37:03 +0400

    feat: add base layout

--- scripts/app.js
+++ scripts/app.js
@@ -6,20 +6,34 @@ canvas.height = canvasSize;
 
 let ctx = canvas.getContext('2d');
 
-let m = m4x4;
 // let m = m32x32;
-let blockCount = m.length;
-let blockSize = canvasSize / blockCount;
+let m = m4x4;
+drawMatrix(m);
 
- for (let i = 0; i < blockCount; i++) {
-    for (let j = 0; j < blockCount; j++) {
-        let data = m[i][j];
-        if (Array.isArray(data)) {
-            ctx.fillStyle = 'rgba(' + data + ')';
-        } else if (typeof data == 'string') {
-            ctx.fillStyle = '#' + data;
+// let src = "./data/image.png";
+// drawImage(src);
+
+function drawMatrix(m) {
+    let blockCount = m.length;
+    let blockSize = canvasSize / blockCount;
+
+    for (let i = 0; i < blockCount; i++) {
+        for (let j = 0; j < blockCount; j++) {
+            let data = m[i][j];
+            if (Array.isArray(data)) {
+                ctx.fillStyle = 'rgba(' + data + ')';
+            } else if (typeof data == 'string') {
+                ctx.fillStyle = '#' + data;
+            }
+            ctx.fillRect(i * blockSize, j * blockSize, blockSize, blockSize);
         }
-        ctx.fillRect(i * blockSize, j * blockSize, blockSize, blockSize);
     }
 }
 
+function drawImage(src) {
+    let image = new Image();
+    image.src = src;
+    image.onload = () => {
+        ctx.drawImage(image, 0, 0, canvasSize, canvasSize);
+    }
+}
