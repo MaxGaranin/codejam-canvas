25337cf16724becc8f23c238f1a9551d0389983a
Max Garanin <m_garanin@mail.ru>
2019-10-28 00:12:26 +0400

    feat: add selection for switcher

--- scripts/app.js
+++ scripts/app.js
@@ -11,15 +11,26 @@ drawMatrix(m4x4);
 m4.addEventListener('click', (event) => {
     drawMatrix(m4x4);
     event.preventDefault();
+    selectItem(event);
 });
 
 m32.addEventListener('click', () => {
     drawMatrix(m32x32);
     event.preventDefault();
+    selectItem(event);
 });
 
 image256.addEventListener('click', () => {
     let src = "./data/image.png";
     drawImage(src);
     event.preventDefault();
+    selectItem(event);
 });
+
+function selectItem(event) {
+    let ul = event.target.closest('ul');
+    for (const li of ul.children) {
+        li.classList.remove('selected');
+    }
+    event.target.closest('li').classList.add('selected');
+}
