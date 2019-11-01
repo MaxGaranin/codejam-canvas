001cad8377f5cb8f74c1153034c35ece63938daa
GaraninMS <GaraninMS@samnipineft.ru>
2019-11-01 08:56:20 +0400

    refactor: fix js issues (rework function hexToRgba, use  Array.prototype.flat)

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -2,24 +2,46 @@ export function drawMatrix(matrix, canvas, ctx) {
     canvas.width = matrix.length;
     canvas.height = matrix.length;
 
-    let colorValues = matrix.reduce(concat);
-    if (typeof(colorValues[0]) === 'string') {
+    let colorValues = matrix.flat();
+    if (typeof (colorValues[0]) === 'string') {
         colorValues = colorValues.map(hexColor => hexToRgba(hexColor));
     }
-    colorValues = colorValues.reduce(concat);
+    colorValues = colorValues.flat();
 
     const imgData = new ImageData(Uint8ClampedArray.from(colorValues), matrix.length, matrix.length);
     ctx.putImageData(imgData, 0, 0);
 }
 
-const concat = (xs, ys) => xs.concat(ys);
+const hexToRgba = hexColor => {
+    hexColor = hexColor.replace('#', '');
+    if (hexColor.length < 6) {
+        hexColor = modifyHex(hexColor);
+    }
 
-const hexToRgba = hexColor => [
-    parseInt(hexColor.slice(0, 2), 16),
-    parseInt(hexColor.slice(2, 4), 16),
-    parseInt(hexColor.slice(4, 6), 16),
-    255
-];
+    const modifyHex = (hexColor) => {
+        if (hexColor.length == 4) {
+            hexColor = hexColor.replace('#', '');
+        }
+        if (hexColor.length == 3) {
+            hexColor =
+                hexColor[0] + hexColor[0] +
+                hexColor[1] + hexColor[1] +
+                hexColor[2] + hexColor[2];
+        }
+        return hexColor;
+    };
+
+    const parseColor = (start, end) => {
+        return parseInt(hexColor.slice(start, end), 16);
+    };
+
+    return [
+        parseColor(0, 2),
+        parseColor(2, 4),
+        parseColor(4, 6),
+        255
+    ];
+};
 
 export function drawImage(src, canvas, ctx) {
     const image = new Image();
